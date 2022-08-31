9797dd0f2e1132b9b92326c27896e7491ae527a8
Aourbro <69416959+Aourbro@users.noreply.github.com>
2022-08-31 16:17:43 +0800

    Add files via upload

--- autoformat.cpp
+++ autoformat.cpp
@@ -21,21 +21,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <vector>
+
 extern "C" {
 
-static uint8_t *buf_base = 0;
-static uint64_t buf_len = 0;
+struct buf_struct{
+    uint8_t *buf_base;
+    uint64_t buf_len;
+    buf_struct *next;
+};
+
+static buf_struct* buf_bases_head = 0;
 static bool start_logging = 0;
 static uint64_t *call_stack = 0;
 static uint64_t call_stack_len = 0;
 static uint64_t call_stack_ptr = 0;
 FILE *fp;
 
-void autoformat_beginning(uint8_t *base, uint64_t len) {
+void autoformat_beginning() {
     assert(start_logging == 0 && "we assume no recursive call on the entry func");
-    buf_base = base;
-    buf_len = len;
     start_logging = 1;
+    buf_struct *buf = (buf_struct *) malloc(sizeof(buf_struct));
+    buf->buf_len = -1;
+    buf->next = NULL;
+    buf_bases_head = buf;
 
     call_stack = (uint64_t *) malloc(1000);
     call_stack_len = 1000;
@@ -48,28 +57,73 @@ void autoformat_beginning(uint8_t *base, uint64_t len) {
 void autoformat_end() {
     assert(start_logging);
     assert(call_stack_ptr == 1);
-    buf_base = 0;
-    buf_len = 0;
     start_logging = 0;
     free(call_stack);
 
     fclose(fp);
 }
 
+void autoformat_add_base(uint8_t *base, uint8_t *src, uint64_t len){
+    if (!start_logging) return;
+
+    buf_struct *crt = buf_bases_head->next;
+    if(!crt){
+        buf_struct *buf = (buf_struct *) malloc(sizeof(buf_struct));
+        buf->buf_base = base;
+        buf->buf_len = len;
+        buf->next = buf_bases_head->next;
+        buf_bases_head->next = buf;
+        return;
+    }
+    while(crt){
+        uint64_t delta = ((uint64_t) src) - (uint64_t) crt->buf_base;
+        if (delta < crt->buf_len) {
+            buf_struct *buf = (buf_struct *) malloc(sizeof(buf_struct));
+            buf->buf_base = base;
+            buf->buf_len = len;
+            buf->next = buf_bases_head->next;
+            buf_bases_head->next = buf;
+            return;
+        }
+        crt = crt->next;
+    }
+}
+
+void autoformat_remove_base(uint8_t *base){
+    if (!start_logging) return;
+
+    buf_struct *prev = buf_bases_head;
+    buf_struct *crt = buf_bases_head->next;
+    while(crt){
+        if(crt->buf_base == base){
+            prev->next = crt->next;
+            crt->next = NULL;
+            crt = prev->next;
+            continue;
+        }
+        prev = prev->next;
+        crt = crt->next;
+    }
+}
+
 void autoformat_logging(uint8_t *ptr, uint64_t nbytes) {
     if (!start_logging) return;
 
     for (unsigned x = 0; x < nbytes; ++x) {
-        uint64_t delta = ((uint64_t) ptr) + x - (uint64_t) buf_base;
-        if (delta < buf_len) {
-            // logging the offset and the call stack
-            fprintf(fp, "[autoformat] %llu, ", delta);
-	    //char c = ptr[x];
-            //fprintf(fp, "%c, ", c);
-            for (unsigned y = 0; y < call_stack_ptr; ++y) {
-                fprintf(fp, "%llu, ", call_stack[y]);
+        buf_struct *crt = buf_bases_head->next;
+        while(crt){
+            uint64_t delta = ((uint64_t) ptr) + x - (uint64_t) crt->buf_base;
+            if (delta < crt->buf_len) {
+                // logging the offset and the call stack
+                fprintf(fp, "[autoformat] %llu, ", delta);
+                char c = ptr[x];
+                fprintf(fp, "%c, ", c);
+                for (unsigned y = 0; y <= call_stack_ptr; ++y) {
+                    fprintf(fp, "%llu, ", call_stack[y]);
+                }
+                fprintf(fp, "\n");
             }
-            fprintf(fp, "\n");
+            crt = crt->next;
         }
     }
 }
