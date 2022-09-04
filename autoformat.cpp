8c4dc97e63310f9810bdfbfd82ea2504bbee9072
Aourbro <69416959+Aourbro@users.noreply.github.com>
2022-09-04 16:48:26 +0800

    Add files via upload

--- autoformat.cpp
+++ autoformat.cpp
@@ -20,6 +20,8 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
 
 #include <vector>
 
@@ -28,6 +30,7 @@ extern "C" {
 struct buf_struct{
     uint8_t *buf_base;
     uint64_t buf_len;
+    uint64_t buf_offset;
     buf_struct *next;
 };
 
@@ -43,6 +46,7 @@ void autoformat_beginning() {
     start_logging = 1;
     buf_struct *buf = (buf_struct *) malloc(sizeof(buf_struct));
     buf->buf_len = -1;
+    buf->buf_offset = -1;
     buf->next = NULL;
     buf_bases_head = buf;
 
@@ -63,35 +67,16 @@ void autoformat_end() {
     fclose(fp);
 }
 
-void autoformat_add_base(uint8_t *base, uint8_t *src, uint64_t len){
+void autoformat_add_base(uint8_t *base, uint64_t off, uint64_t len){
     if (!start_logging) return;
 
-    buf_struct *crt = buf_bases_head->next;
-    if(!crt){
-        buf_struct *buf = (buf_struct *) malloc(sizeof(buf_struct));
-        buf->buf_base = base;
-        buf->buf_len = len;
-        buf->next = buf_bases_head->next;
-        buf_bases_head->next = buf;
-        return;
-    }
-    while(crt){
-        if(crt->buf_base == base) return;
-        crt = crt->next;
-    }
-    crt = buf_bases_head->next;
-    while(crt){
-        uint64_t delta = ((uint64_t) src) - (uint64_t) crt->buf_base;
-        if (delta < crt->buf_len && crt->buf_base != base) {
-            buf_struct *buf = (buf_struct *) malloc(sizeof(buf_struct));
-            buf->buf_base = base;
-            buf->buf_len = len;
-            buf->next = buf_bases_head->next;
-            buf_bases_head->next = buf;
-            return;
-        }
-        crt = crt->next;
-    }
+    buf_struct *buf = (buf_struct *) malloc(sizeof(buf_struct));
+    buf->buf_base = base;
+    buf->buf_len = len;
+    buf->buf_offset = off;
+    buf->next = buf_bases_head->next;
+    buf_bases_head->next = buf;
+    return;
 }
 
 void autoformat_remove_base(uint8_t *base){
@@ -120,7 +105,7 @@ void autoformat_logging(uint8_t *ptr, uint64_t nbytes) {
             uint64_t delta = ((uint64_t) ptr) + x - (uint64_t) crt->buf_base;
             if (delta < crt->buf_len) {
                 // logging the offset and the call stack
-                fprintf(fp, "[autoformat] %llu, ", delta);
+                fprintf(fp, "[autoformat] %llu, ", delta + crt->buf_offset);
                 char c = ptr[x];
                 fprintf(fp, "%c, ", c);
                 for (unsigned y = 0; y < call_stack_ptr; ++y) {
@@ -147,6 +132,59 @@ void autoformat_popping() {
     if (!start_logging) return;
     assert(call_stack_ptr > 0);
     call_stack_ptr--;
+}
+
+void autoformat_memcpy(uint8_t *dest, uint8_t *src, uint64_t size){
+    if (!start_logging) return;
+    buf_struct *crt = buf_bases_head->next;
+
+    while(crt){
+        uint64_t delta = ((uint64_t) src) - (uint64_t) crt->buf_base;
+        if (delta < crt->buf_len) {
+            buf_struct *crt1 = buf_bases_head->next;
+            while(crt1){
+                if(crt1->buf_base == dest){
+                    crt1->buf_offset = crt->buf_offset + delta;
+                    return;
+                }
+                crt1 = crt1->next;
+            }
+            autoformat_add_base(dest, delta + crt->buf_offset, size);
+            return;
+        }
+        crt = crt->next;
+    }
+}
+
+void autoformat_sscanf(char *str, char *format, ...){
+    if (!start_logging) return;
+    autoformat_logging((uint8_t *) str, strlen(str));
+
+    va_list arg_ptr;
+    va_start(arg_ptr, format);
+    int str_len = strlen(str);
+    int fmt_len = strlen(format);
+    char *buffer = (char *) malloc(fmt_len);
+    uint64_t buf_ptr = 0;
+    uint64_t str_ptr = 0;
+    for(int i = 0; i < fmt_len; ++i, ++str_ptr){
+        if(format[i] != '%') buffer[buf_ptr++] = str[i];
+        else{
+            ++i;
+            if(format[i] == '%'){
+                buffer[buf_ptr++] = format[i];
+            }
+            else{
+                autoformat_logging((uint8_t *) buffer, buf_ptr);
+                memset(buffer, 0, strlen(buffer));
+                buf_ptr = 0;
+                assert(format[i] == 's');
+                char *base = va_arg(arg_ptr, char*);
+                autoformat_memcpy((uint8_t *) base, (uint8_t *)(str + str_ptr), strlen(base));
+                str_ptr += strlen(base) - 1;
+            }
+        }
+    }
 }
 
 }
